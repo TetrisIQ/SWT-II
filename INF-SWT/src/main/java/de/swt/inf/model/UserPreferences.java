513b4f468207fa0da0c08c413389289af91f6675
Alex <TetrisIQ@web.de>
2018-04-16 11:59:44 +0200

    Added Old Source

---
+++ INF-SWT/src/main/java/de/swt/inf/model/UserPreferences.java
@@ -0,0 +1,55 @@
+package de.swt.inf.model;
+
+public class UserPreferences {
+
+    private int USER_PREFERENCES_ID;
+
+    private int semester;
+
+    private int age;
+
+    private User user;
+
+    private CourseEnum courseEnum;
+
+    private ProvinceEnum provinceEnum;
+
+    private UniversityEnum universityEnum;
+
+    private MusicPreferencesEnum musicPreferencesEnum;
+
+    private GenderEnum genderEnum;
+
+    public UserPreferences() {
+
+    }
+
+    public void setProvince(ProvinceEnum province) {
+        //TO DO
+    }
+
+    public void setUniversity(UniversityEnum university) {
+        //TO DO
+    }
+
+    public void setCourse(CourseEnum course) {
+        //TO DO
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public void setMusic(MusicPreferencesEnum music) {
+        //TO DO
+    }
+
+    public void setGender(GenderEnum gender) {
+        //TO DO
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
