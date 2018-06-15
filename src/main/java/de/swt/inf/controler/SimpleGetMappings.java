8cf3cb37215a6b29a37b501507f25a3f26a15936
PWire <philipp.drath@stud.fh-luebeck.de>
2018-06-15 11:25:13 +0200

    #4 Logout Deleting Cookie after Logout

--- src/main/java/de/swt/inf/controler/SimpleGetMappings.java
+++ src/main/java/de/swt/inf/controler/SimpleGetMappings.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class SimpleGetMappings {
@@ -25,6 +28,23 @@ public class SimpleGetMappings {
     @RequestMapping(value="/login")
     public String login() {
         return "login";
+    }
+
+    @RequestMapping(value="/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
+        for(int i=0; i< cookies.length; i++) {
+            if(cookies[i].getName().equals("login")){
+                cookies[i].setMaxAge(0);
+                response.addCookie(cookies[i]);
+                System.out.println("Successfully logged out!");
+                try {
+                    response.sendRedirect("/login");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
