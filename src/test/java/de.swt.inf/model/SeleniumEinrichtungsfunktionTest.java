cd10412c615cc4fdf0d6b487dd9224102f6fe273
Nina Klosterkötter <nina.klosterkoetter@stud.fh-luebeck.de>
2018-06-18 00:37:26 +0200

    #23 Einrichtungsfunktion
    Seleniumtest überarbeitet/HTML angepasst

--- src/test/java/de.swt.inf/model/SeleniumEinrichtungsfunktionTest.java
+++ src/test/java/de.swt.inf/model/SeleniumEinrichtungsfunktionTest.java
@@ -47,24 +47,51 @@ public class SeleniumEinrichtungsfunktionTest {
         Select study = new Select(driver.findElement(By.name("course")));
         study.selectByVisibleText("Informatik_I");
 
+        Select semester = new Select(driver.findElement(By.name("semester")));
+        semester.selectByVisibleText("4");
+
         Select uni = new Select(driver.findElement(By.name("university")));
         uni.selectByVisibleText("Uni_Hamburg");
 
         Select province = new Select(driver.findElement(By.name("province")));
         province.selectByVisibleText("Schleswig_Holstein");
 
+        Select age = new Select(driver.findElement(By.name("age")));
+        age.selectByVisibleText("unter 30");
+
         element = driver.findElement(By.name("speichern"));
         element.click();
 
         System.out.println("Name:" + driver.getCurrentUrl());
         System.out.println("Title is: " + driver.getTitle());
-        System.out.println("Termin: " + driver.findElement(By.linkText("Deichbrand Festival")).getText());
 
-        String strg = driver.findElement(By.linkText("Deichbrand Festival")).getText();
+        String strng = driver.findElement(By.linkText("Deichbrand Festival")).getText();
+        assertEquals("Dashboard/Woche", driver.getTitle());
+        assertEquals("http://localhost:8080/dashboard/woche", driver.getCurrentUrl());
+        assertEquals("Deichbrand Festival", strng);
+
+        //System.out.println("Termin: " + driver.findElement(By.linkText("Deichbrand Festival")).getText());
+        WebElement SearchFestival = driver.findElement(By.linkText("Deichbrand Festival"));
+        SearchFestival.click();
+
+
+        //assertEquals("Deichbrand Festival", driver.findElement(By.name("name")).getAttribute("Deichbrand Festival"));
+        assertEquals("Termin Bearbeiten", driver.getTitle());
+
+        System.out.println("Name: " +driver.getCurrentUrl());
+        System.out.println("Title: " + driver.getTitle());
+
+        WebElement elementBack = driver.findElement(By.name("speichern"));
+        elementBack.click();
+
+        System.out.println("Name: " +driver.getCurrentUrl());
+        System.out.println("Title: " + driver.getTitle());
+
+       String strg = driver.findElement(By.linkText("Deichbrand Festival")).getText();
         //JUnit-Test, ob der Termin mit dem Festival erstellt wurde
-        assertEquals("http://localhost:8080/dashboard", driver.getCurrentUrl());
-        assertEquals("Dashboard", driver.getTitle());
-        assertEquals("Deichbrand Festival", strg);
+       assertEquals("http://localhost:8080/dashboard/woche", driver.getCurrentUrl());
+       assertEquals("Dashboard/Woche", driver.getTitle());
+       assertEquals("Deichbrand Festival", strg);
     }
 }
 
